e4bff485107606884647928bae9d0969d8c0f314
Paras Chandra <paraschandra30@gmail.com>
2024-01-03 11:13:11 +0530

    login comp added

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Container from './container/Container'
 import Logo from './Logo'
+import Button from './Button'
+import Input from './Input'
 
-export {Header, Footer, Container, Logo}
+export {Header, Footer, Container, Logo, Button, Input}
