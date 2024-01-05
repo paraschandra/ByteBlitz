8f9b1abc7fed26c0435759801f15e330beb7140e
Paras Chandra <paraschandra30@gmail.com>
2024-01-05 09:55:13 +0530

    pages added

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,11 @@ import Button from './Button'
 import Input from './Input'
 import Select from './Select'
 import RTE from './RTE'
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from './postForm/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import LogoutBtn from './Header/LogoutBtn'
 
-export {Header, Footer, Container, Logo, Button, Input, Select, RTE}
+export {Header, Footer, Container, Logo, Button, Input, Select, RTE, Signup, Login, PostCard, PostForm, AuthLayout, LogoutBtn}
